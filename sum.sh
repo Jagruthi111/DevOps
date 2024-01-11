3f4717a4ea7709b9f9e005318c6dd301682f80b7
Jagruthi111 <154959968+Jagruthi111@users.noreply.github.com>
2024-01-11 15:19:20 +0530

    Update sum.sh

--- sum.sh
+++ sum.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-echo "Enter the number to find sum of first n numbers"
-read num
+#echo "Enter the number to find sum of first n numbers"
+#read num
+echo $1
+echo $num
 sum=0
 while [ $num -gt 0 ]
 do
