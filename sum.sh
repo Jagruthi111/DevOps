cb055db836e70aae496819e01627a0fdcd8029ad
Jagruthi111 <154959968+Jagruthi111@users.noreply.github.com>
2024-01-11 16:21:01 +0530

    Update sum.sh

--- sum.sh
+++ sum.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #echo "Enter the number to find sum of first n numbers"
 #read num
-echo " cmd line argument $1"
+#echo " cmd line argument $1"
 echo " parameter num inside inside $num"
 sum=0
 while [ $num -gt 0 ]
